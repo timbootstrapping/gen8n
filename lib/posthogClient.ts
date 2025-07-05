0335458b7ce4682ff671bc11bc8335cf58cb7803
TimKramny <tim.ebusiness@web.de>
2025-07-05 16:40:42 +0200

    Add: Added Reverse Proxy for Posthog

--- lib/posthogClient.ts
+++ lib/posthogClient.ts
@@ -2,7 +2,7 @@ import posthog from 'posthog-js';
 
 if (typeof window !== 'undefined' && !posthog.__loaded) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: '/ph-proxy-8n/',
   });
   (posthog as any).__loaded = true; // Prevent double init
 }
